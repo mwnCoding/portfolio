2e3b71a5d516562142955951667627828610869e
mwnCoding <MaximilianoNaddeo@gmail.com>
2024-02-08 12:12:39 +0100

    setup basic homepage

---
+++ portfolio/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+function HomePage() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>Welcome!</h1>
+        <p>
+          My name is Maximiliano Naddeo, I am a front-end developer based in
+          Wörgl, Tyrol, Austria. I currently have one project to showcase which
+          I created during my web developement bootcamp with Ironhack.
+        </p>
+        <div>
+          <h2>Checkout my latest projects:</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;

b36a06854d4a6627c4ec236164e2d99b2a68d173
mwnCoding <MaximilianoNaddeo@gmail.com>
2024-02-11 18:16:18 +0100

    add links footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,26 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <footer
       id="footer"
       className="flex justify-center items-center bg-black text-white w-screen mt-auto py-4"
     >
-      <span className="mx-2">home</span>
-      <span className="mx-2">about</span>
-      <span className="mx-2">projects</span>
-      <span className="mx-2">cv</span>
-      <span>contact</span>
+      <Link to="/">
+        <span className="mx-2">home</span>
+      </Link>
+      <Link to="/about">
+        <span className="mx-2">about</span>
+      </Link>
+      <Link to="/projects">
+        <span className="mx-2">projects</span>
+      </Link>
+      <Link to="/cv">
+        <span className="mx-2">cv</span>
+      </Link>
+      <Link to="contact">
+        <span>contact</span>
+      </Link>
     </footer>
   );
 }

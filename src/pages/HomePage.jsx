5d47ca0ab65de4fd683a456c8e21cc1e07185ef4
mwnCoding <MaximilianoNaddeo@gmail.com>
2024-02-11 18:35:43 +0100

    add latest project section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,54 +4,29 @@ function HomePage() {
   return (
     <>
       <div className="flex flex-col items-center mb-10 mx-12">
-        <h1 className="text-3xl font-bold mb-2">Welcome!</h1>
-        <p className="mb-12">
+        <h1 className="text-3xl font-bold mb-2 text-center">Welcome!</h1>
+        <p className="mb-12 text-center">
           My name is Maximiliano Naddeo, I am a front-end developer based in
           Wörgl, Tyrol, Austria. I currently have one project to showcase which
           I created during my web developement bootcamp with Ironhack.
         </p>
         <div>
           <hr className="mb-10" />
-          <h2 className="text-2xl font-bold">Checkout my latest projects:</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-            molestiae aliquam consequatur inventore veritatis quas consectetur
-            dignissimos quibusdam, itaque mollitia temporibus cum, harum
-            explicabo dolore dolorum esse molestias quis quisquam? Quaerat
-            placeat animi quibusdam quam fuga dicta ratione a illo, consequatur
-            nam commodi, nisi maiores aliquam dolor magni quas libero
-            necessitatibus velit laboriosam reiciendis? Aperiam sequi provident
-            fuga alias amet! Ad ex quia vitae? Aliquam tempore optio saepe ea
-            nostrum, quas veritatis ipsum dolore. Earum cumque quasi dignissimos
-            minus exercitationem a, placeat veritatis adipisci neque quis autem,
-            impedit ipsa excepturi. Itaque aliquid ipsum perferendis repellendus
-            eveniet nisi nesciunt veritatis sint, nostrum magni explicabo quis
-            facere voluptates consequuntur nulla earum neque repudiandae
-            corporis nihil et officiis asperiores facilis perspiciatis
-            similique! Debitis. Unde velit sapiente sed at dolorum alias ad
-            vitae voluptatum ipsum laboriosam, dolore veritatis, quasi dicta
-            veniam harum perspiciatis consequuntur, fugit ratione reiciendis
-            eligendi quod soluta. Quia repellendus nesciunt asperiores. Ex
-            doloremque consequuntur saepe est, corporis possimus nesciunt
-            consectetur id eligendi deserunt repudiandae aliquid, modi dolore
-            voluptates eaque nemo dolores alias nihil? Vel reiciendis molestias
-            similique reprehenderit soluta, aperiam iste. Veniam, blanditiis
-            numquam corrupti, sapiente inventore minus necessitatibus laudantium
-            quibusdam veritatis possimus explicabo consequatur? Repellat,
-            corporis? Quisquam nulla doloribus delectus. Sapiente qui dicta
-            sequi accusantium quo, iusto corrupti iste ea. Doloribus quis
-            ratione delectus nemo quos, ut excepturi ipsa quibusdam fugit id
-            reprehenderit, voluptates minus impedit dolor soluta perspiciatis
-            accusantium optio odio temporibus laudantium. Voluptatum, omnis! Ex
-            facilis quo recusandae? Qui explicabo nisi sit quis maiores,
-            voluptatibus quos ex delectus praesentium iste vel fugiat reiciendis
-            et nesciunt animi. Maiores optio et atque corrupti consequatur eius.
-            Nulla, repellendus. Fugiat, molestias aliquam. Fugiat corrupti
-            repellat nisi eos et, ab officiis asperiores expedita error, omnis
-            explicabo laboriosam quasi modi perferendis doloribus, voluptate
-            maiores magnam? Quis exercitationem unde dolor eaque vel! Dolorum,
-            omnis temporibus.
-          </p>
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            Checkout my latest project:
+          </h2>
+          <div className="border-black border-2 flex flex-col items-center">
+            <h3 className="font-bold underline mb-2">Renting Stuff Online</h3>
+            <p className="leading-5 text-center mb-3">
+              Renting Stuff Online is a community based renting platform for
+              sports equipment. Rent stuff to and from other people, leave
+              comments and rate your experience
+            </p>
+            <div className="flex justify-around w-full mb-3">
+              <button>Go to site</button>
+              <button>Details</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
